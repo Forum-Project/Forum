8cada2ea10105a481f4bd46a86e7033c092d9a2f
Timothy McGowen <mcgowen17@yahoo.com>
2019-11-12 16:54:21 -0700

    Added Signup Functionality, getting a 404 on backend

--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+// import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -12,6 +11,8 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+
+import axios from 'axios'
 
 function Copyright() {
     return (
@@ -53,6 +54,25 @@ const useStyles = makeStyles(theme => ({
 
 export default function SignUp() {
     const classes = useStyles();
+    const [user, setUser] = useState({});
+
+    const onSumbit = event => {
+        if (user.username && user.email && user.password) {
+            event.preventDefault();
+            console.log('This is what is being sent to ', user)
+            axios.post('http://localhost:5000/', user)
+                .then(res => console.log('You created a user!', res))
+                .catch(err => console.log('You failed to Create a New User', err))
+        } else {
+            console.log('Validate')
+        }
+    }
+
+    const handleChange = event => {
+        const updatedUser = { ...user, [event.target.name]: event.target.value }
+        setUser(updatedUser)
+        console.log(updatedUser)
+    }
 
     return (
         <Container component="main" maxWidth="xs">
@@ -64,29 +84,18 @@ export default function SignUp() {
                 <Typography component="h1" variant="h5">
                     Sign up
         </Typography>
-                <form className={classes.form} noValidate>
+                <form className={classes.form} Validate>
                     <Grid container spacing={2}>
-                        <Grid item xs={12} sm={6}>
-                            <TextField
-                                autoComplete="fname"
-                                name="firstName"
-                                variant="outlined"
-                                required
-                                fullWidth
-                                id="firstName"
-                                label="First Name"
-                                autoFocus
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6}>
+                        <Grid item xs={12}>
                             <TextField
                                 variant="outlined"
                                 required
                                 fullWidth
-                                id="lastName"
-                                label="Last Name"
-                                name="lastName"
-                                autoComplete="lname"
+                                id="Username"
+                                label="Username"
+                                name="username"
+                                autoComplete="Username"
+                                onChange={handleChange}
                             />
                         </Grid>
                         <Grid item xs={12}>
@@ -98,6 +107,7 @@ export default function SignUp() {
                                 label="Email Address"
                                 name="email"
                                 autoComplete="email"
+                                onChange={handleChange}
                             />
                         </Grid>
                         <Grid item xs={12}>
@@ -110,14 +120,10 @@ export default function SignUp() {
                                 type="password"
                                 id="password"
                                 autoComplete="current-password"
+                                onChange={handleChange}
                             />
                         </Grid>
-                        <Grid item xs={12}>
-                            <FormControlLabel
-                                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                                label="I want to receive inspiration, marketing promotions and updates via email."
-                            />
-                        </Grid>
+
                     </Grid>
                     <Button
                         type="submit"
@@ -125,12 +131,13 @@ export default function SignUp() {
                         variant="contained"
                         color="primary"
                         className={classes.submit}
+                        onClick={onSumbit}
                     >
                         Sign Up
           </Button>
                     <Grid container justify="flex-end">
                         <Grid item>
-                            <Link href="#" variant="body2">
+                            <Link href="" variant="body2">
                                 Already have an account? Sign in
               </Link>
                         </Grid>
