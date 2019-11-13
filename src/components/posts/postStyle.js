f1a88691a6610be3cc46200115e12f41863dc755
Arvin Agas <arvagas@gmail.com>
2019-11-13 18:11:44 -0500

    created post page

---
+++ src/components/posts/postStyle.js
@@ -0,0 +1,20 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { green, grey } from '@material-ui/core/colors';
+
+export const postStyle = makeStyles(theme => ({
+  card: {
+    maxWidth: 1000,
+    backgroundColor: grey[300],
+    textAlign: 'left',
+    margin: '2rem',
+  },
+  body: {
+    backgroundColor: grey[100],
+  },
+  avatar: {
+    backgroundColor: green[600],
+  },
+  tag: {
+    fontSize: '0.750rem'
+  },
+}));
