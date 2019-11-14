35ef0762257ef3c8ff5c5c8c70e49e1716e1f31f
Arvin Agas <arvagas@gmail.com>
2019-11-14 12:50:53 -0500

    post now relies on being used in postpage

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -14,13 +14,9 @@ import axios from 'axios'
 // css and styles
 import { postStyle } from './postStyle'
 
-//post preview will be wrapped in a link that has props passed down to this component?
 const Post = (props) => {
   // const { post } = props
   const classes = postStyle();
-  const [comments, setComments] = useState([])
-  //this is a counter dedicated to running the useEffect whenever a new comment is made
-  const [useEffectCount, setUseEffectCount] = useState(0)
   const post = {
     post_title: "Another test post",
     post_body: 'this is a really long body description just to test if i can use a function that limits the amount of characters that will be seen in the post body. We want the user to be enticed by what is about to be said in the post so that they have to click on it and be redirected to the entirety of the post',
@@ -28,66 +24,44 @@ const Post = (props) => {
     post_category: ['test', 'moretests', 'evenmoretest', 'plsno'],
   }
 
-  //this makes an api call to the backend to grab all comments associated with the post; will rerun if a new comment has been posted
-  // useEffect(() => {
-  //   axios.get(`https://backend.web/posts/${post._id}/comments`)
-  //   .then(res => setComments(res.data))
-  //   .catch(err => alert('There was an error retrieving the comments.'))
-  // }, [useEffectCount])
-
   return (
-    <Container className={classes.container}>
-      <Card className={classes.card}>
-        <CardHeader
-          avatar={
-            <Avatar aria-label="recipe" className={classes.avatar}>
-              {/* profile image here, otherwise initials of person? */}
-              R
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title={post.post_title}
-          titleTypographyProps={{ variant: 'h4' }}
-          subheader={
-            <Box>
-              {post.post_date}
-              <Box display='flex' justifyContent="flex-end">
-                {/* map over tags here */}
-                {post.post_category.map(tag => {
-                  return (
-                    <Button className={classes.tag} onClick={() => console.log(`${tag} brings you to a different page with only ${tag}-related results`)}>
-                      #{tag}
-                    </Button>
-                  )
-                })}
-              </Box>
+    <Card className={classes.card}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            {/* profile image here, otherwise initials of person? */}
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={post.post_title}
+        titleTypographyProps={{ variant: 'h4' }}
+        subheader={
+          <Box>
+            {post.post_date}
+            <Box display='flex' justifyContent="flex-end">
+              {/* map over tags here */}
+              {post.post_category.map(tag => {
+                return (
+                  <Button className={classes.tag} onClick={() => console.log(`${tag} brings you to a different page with only ${tag}-related results`)}>
+                    #{tag}
+                  </Button>
+                )
+              })}
             </Box>
-          }
-        />
-        <CardContent className={classes.body}>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {post.post_body}
-          </Typography>
-        </CardContent>
-      </Card>
-
-      {/* form text area for adding comments */}
-      {/* <NewComment useEffectCount={useEffectCount} setUseEffectCount={setUseEffectCount} /> */}
-      <div>New comment text area just under here</div>
-
-      
-      {/* map over comments here */}
-      {/* {comments.map(comment => {
-        return (
-          <Comment comment={comment}/>
-        )
-      })} */}
-      <div>Existing comments down here</div>
-    </Container>
+          </Box>
+        }
+      />
+      <CardContent className={classes.body}>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {post.post_body}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 }
 
