b3099fb1546048b633f4e5714b3484c10be5e6c9
Timothy McGowen <mcgowen17@yahoo.com>
2019-11-15 09:43:13 -0700

    merge from master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react';
 
-import { Route } from 'react-router-dom'; 
+import { Route } from 'react-router-dom';
 
 // component imports
 import Routes from './routes/Routes';
-import Signin from './components/signin/Signin'; 
-import Signup from './components/signup/Signup'; 
-
+import Signin from './components/signin/Signin';
+import Signup from './components/signup/Signup';
+import Navbar from '../src/components/Navbar/Navbar'
 
 import './App.css';
 
 function App() {
   return (
     <>
-		{/* <TabPanel /> */}
-    {/* <Navbar /> */}
-    <Route exact path="/" component={Signup} />
-    <Route path="/signin" component={Signin} />
-    <Routes />
+      {/* <TabPanel /> */}
+      <Route exact path="/" component={Signup} />
+      <Route path="/signin" component={Signin} />
+      <Routes />
     </>
   );
 }
