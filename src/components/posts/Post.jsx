b89996419473ebdc17b8eb6f9cf527915bcbaace
Arvin Agas <arvagas@gmail.com>
2019-11-15 14:08:46 -0500

    post data is pulled and is used

--- src/components/posts/Post.jsx
+++ src/components/posts/Post.jsx
@@ -1,28 +1,34 @@
 // library imports
-import React, { useEffect, useState } from 'react';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardContent from '@material-ui/core/CardContent';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import React, { useEffect, useState } from 'react'
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import CardContent from '@material-ui/core/CardContent'
+import Avatar from '@material-ui/core/Avatar'
+import IconButton from '@material-ui/core/IconButton'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import Box from '@material-ui/core/Box'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
 import axios from 'axios'
 // css and styles
 import { postStyle } from './postStyle'
 
 const Post = (props) => {
   // const { post } = props
-  const classes = postStyle();
-  const post = {
-    post_title: "Another test post",
-    post_body: 'this is a really long body description just to test if i can use a function that limits the amount of characters that will be seen in the post body. We want the user to be enticed by what is about to be said in the post so that they have to click on it and be redirected to the entirety of the post',
-    post_date: 'November 12, 2019',
-    post_category: ['test', 'moretests', 'evenmoretest', 'plsno', 'what', 'whatever', 'duh'],
-  }
+  const [post, setPost] = useState({ _id: '5dceeead7bd912553c9fc24c' }) //delete once we have props working and uncomment aboove
+  const [pulledPost, setPulledPost] = useState({})
+  const classes = postStyle()
+  const backend = 'http://localhost:5000'
+
+  useEffect(() => {
+    if (post._id) {
+      axios.get(`${backend}/posts/${post._id}`)
+      .then(res => {
+        setPulledPost(res.data)
+      })
+      .catch(err => console.log('Catch for post was invoked:', err))
+    }
+  }, [])
 
   return (
     <Card className={classes.card}>
@@ -38,14 +44,13 @@ const Post = (props) => {
             <MoreVertIcon />
           </IconButton>
         }
-        title={post.post_title}
+        title={pulledPost.post_title}
         titleTypographyProps={{ variant: 'h4' }}
         subheader={
           <Box>
-            {post.post_date}
+            {pulledPost.post_date}
             <Box display='flex' justifyContent="flex-end" flexWrap='wrap' maxWidth='200' width='100%'>
-              {/* map over tags here */}
-              {post.post_category.map(tag => {
+              {pulledPost.post_tags && pulledPost.post_tags.map(tag => {
                 return (
                   <Button className={classes.tag} onClick={() => console.log(`${tag} brings you to a different page with only ${tag}-related results`)}>
                     #{tag}
@@ -58,11 +63,11 @@ const Post = (props) => {
       />
       <CardContent className={classes.body}>
         <Typography variant="body2" color="textSecondary" component="p">
-          {post.post_body}
+          {pulledPost.post_body}
         </Typography>
       </CardContent>
     </Card>
-  );
+  )
 }
 
 export default Post
