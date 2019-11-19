02c7ac691583431ffdef1d6d6223426162d4a0b3
Arvin Agas <arvagas@gmail.com>
2019-11-19 17:33:16 -0500

    react fragment instead of empty string

--- src/components/comments/CommentsCard.jsx
+++ src/components/comments/CommentsCard.jsx
@@ -129,7 +129,7 @@ export default function CommentsCard(props) {
                             Delete
                         </Button>
                     </>
-                ) : ''}
+                ) : <></>}
             </CardActions>
         </Card>
     );
